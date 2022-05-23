276a65f1e83560cb115cce20078dbde888d7f3e5
alenquer <dev.alenquer@gmail.com>
2022-05-23 17:45:40 -0300

    fix css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,21 +15,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
-            rel="stylesheet"
-            media="print"
-            // @ts-ignore
-            onLoad="this.media='all'"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
